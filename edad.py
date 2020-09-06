8b423ec21d9a6d40047eed13e1e9fda57cf0be4c
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-09-05 19:23:06 -0500

    Creacion test_edad

--- edad.py
+++ edad.py
@@ -1,13 +1,10 @@
-edad_actual = int(input("Ingresa tu edad actual:"))
-año_actual = int(input("Ingresa el año actual:"))
-
-if edad_actual > 0:
-    if año_actual > 0:
-        años = 2070 - año_actual 
-        edad_futura = edad_actual + años
-        
-        print("En el año 2070 vas a tener ", edad_futura)
+def calculo_edad(edad_actual, año_actual):
+    if edad_actual > 0:
+        if año_actual > 0:
+            años = 2070 - año_actual 
+            edad_futura = edad_actual + años
+            return edad_futura
+        else:
+            print("El año ingresado es negativo")
     else:
-        print("El año ingresado es negativo")
-else:
-    print("La edad ingresada es negativa")
+        print("La edad ingresada es negativa")
