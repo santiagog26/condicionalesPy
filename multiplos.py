022ae8c4ab9a411ab8aa91e0a1f91b5deba6ecce
Santiago Gutiérrez Orjuela <santiago.gutierrez02@correo.usa.edu.co>
2020-09-14 11:21:46 -0500

    Verifico que ambos limites sean positivo

--- multiplos.py
+++ multiplos.py
@@ -1,11 +1,12 @@
 def multiplos_rango(limite_inferior, limite_superior, numero):
-    if limite_inferior > limite_superior:
-        limite_inferior, limite_superior = limite_superior, limite_inferior
-    limite_superior += 1
     multiplos = []
-    for i in range(limite_inferior, limite_superior):
-        if (i % numero) == 0:
-            multiplos.append(i)
+    if limite_inferior > 0 and limite_superior > 0:
+        if limite_inferior > limite_superior:
+            limite_inferior, limite_superior = limite_superior, limite_inferior
+        limite_superior += 1
+        for i in range(limite_inferior, limite_superior):
+            if (i % numero) == 0:
+                multiplos.append(i)
     return multiplos
 
 print(multiplos_rango(2,21,3))
--- test_multiplos.py
+++ test_multiplos.py
@@ -9,7 +9,9 @@ def test_multiplos_rango():
     [
         (2,21,3,[3,6,9,12,15,18,21]),
         (2,4,3,[3]),
-        (1,10,2,[2,4,6,8,10])
+        (1,10,2,[2,4,6,8,10]),
+        (-10,2,2,[]),
+        (21,2,3,[3,6,9,12,15,18,21])
     ]
 )
 def test_multiplos_rango_multi(input_a, input_b, input_c, expected):
